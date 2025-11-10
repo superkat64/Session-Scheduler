694a8d105061f0c29c4a603e6c7a9f21d0bc00b2
Kate Piette <kat.piette@gmail.com>
2025-11-10 16:15:57 -0500

    Add  to controllers, update site to use full_name instead of first_name last_name

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :client
   has_one :user, through: :client
   has_many :notes, dependent: :destroy
+  validates :scheduled_at, presence: true
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ApplicationRecord
   belongs_to :user
   has_many :appointments, dependent: :destroy
+  validates :first_name, :last_name, :email, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
