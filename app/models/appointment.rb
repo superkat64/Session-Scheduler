1478dfafc6433c4007806bc3677ccdbb49a183f0
Kate Piette <kat.piette@gmail.com>
2025-11-10 16:45:29 -0500

    Format appointment.scheduled_at

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,8 @@ class Appointment < ApplicationRecord
   has_one :user, through: :client
   has_many :notes, dependent: :destroy
   validates :scheduled_at, presence: true
+
+  def formatted_scheduled_at
+    scheduled_at.strftime("%b %d, %Y at %I:%M %p")
+  end
 end
